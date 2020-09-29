1cd72213d7633f49afe50ce1fda482a5c5fec276
Romano Ferreira <arcferreira@id.uff.br>
2020-09-29 18:23:42 -0300

    busca matricula e cria o Aluno
    logica de busca da matricula e inicializacao do aluno associado.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ class Arquivo:
 	def __init__(self):
 		self.lista_arquivo = []
 
-	def gera_lista_arquivo(self):
-		with open("alunos.csv", "r") as arquivo_base:
+	def gera_lista_arquivo(self):  # metodo que le o arquivo e coloca em memoria
+		with open('Desafio-1-STI/alunos.csv', 'r') as arquivo_base:
 			info_arquivo = csv.reader(arquivo_base)
 			for line in info_arquivo:
 				self.lista_arquivo.append(line)
@@ -23,9 +23,21 @@ class Aluno:
 
 
 arquivo = Arquivo()
-arquivo.gera_lista_arquivo()    # carrega o arquivo em memoria na variavel arquivo.lista_arquivo
+arquivo.gera_lista_arquivo()  # carrega o arquivo em memoria na variavel arquivo.lista_arquivo
 
-for lini in arquivo.lista_arquivo:
-	print(arquivo.lista_arquivo[lini])  # teste
+nova = "S"
 
+while nova != "N":
+	num_matricula = input("Digite seu numero de matricula: \n")
+
+	for each in arquivo.lista_arquivo:  # percorre a lista ate encontrar o numero de matricula informado
+		if each[1] == num_matricula:
+			aluno = Aluno(each[0], each[1], each[2], each[3], each[4], each[5])  # instacia o aluno achado
+			nova = "N"
+
+	if nova == "S":  # pergunta se o usuario deseja fazer nova busca ou fechar o programa
+		print("Aluno nao encontrado, deseja fazer nova pesquisa? S/N \n")
+		nova = input().upper()
+		if nova == "N":
+			quit()
 
